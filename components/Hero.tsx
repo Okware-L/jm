5bda9bca847021e7126bf4287647bac62e5daf89
Okware-L <lewisokware@gmail.com>
2024-04-26 10:24:21 +0300

    new hero responsive

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ export default function Hero() {
   return (
     <div className="h-[95vh] bg-gradient-to-l from-gray-100 to-gray-200  text-slate-900 overflow-y-scroll">
       {/**top */}
-      <div className=" pt-32 px-10 grid md:grid-cols-3 gap-3">
+      <div className=" pt-32 px-10 grid md:grid-cols-3 gap-3 w-full">
         <div className="col-span-2">
           <h1 className="text-align-left text-5xl my-3 tracking-wide">
             The <br />
@@ -25,17 +25,41 @@ export default function Hero() {
             </Button>
           </Link>
         </div>
-        <div className="bg-hero-first bg-center bg-cover rounded-2xl">
+        <div className="bg-hero-first bg-center bg-cover rounded-2xl hidden md:block">
           <div className="hero-overlay bg-opacity-30  rounded-2xl ">
             <h2 className="text-slate-100 text-center font-extralight text-xl pt-3 tracking-widest">
               Smart Investing.
             </h2>
           </div>
         </div>
+
+        <div className="space-y-5 w-96">
+          <div className="bg-hero-first bg-center bg-cover rounded-2xl md:hidden">
+            <div className="hero-overlay bg-opacity-30  rounded-2xl ">
+              <h2 className="text-slate-100 text-center font-extralight text:sm md:text-xl pt-3 tracking-widest p-10">
+                Smart Investing.
+              </h2>
+            </div>
+          </div>
+          <div className="bg-hero-second bg-center bg-cover rounded-2xl md:hidden ">
+            <div className="hero-overlay bg-opacity-50  rounded-2xl">
+              <h2 className="text-slate-100 text-center font-extralight text:sm md:text-xl pt-3 tracking-widest p-10">
+                Start navigating your finance
+              </h2>
+            </div>
+          </div>
+          <div className="bg-hero-third bg-center bg-cover rounded-2xl md:hidden">
+            <div className="hero-overlay bg-opacity-20  rounded-2xl">
+              <h2 className="text-slate-100 text-center font-extralight text:sm md:text-xl pt-3 tracking-widest p-10">
+                Securing Futures
+              </h2>
+            </div>
+          </div>
+        </div>
       </div>
 
       {/**bottom */}
-      <div className="px-10 py-10 grid grid-cols-1 md:grid-cols-3 gap-3">
+      <div className="px-10 py-5 grid grid-cols-1 md:grid-cols-3 gap-3">
         <div>
           <div className="flex py-16">
             <Link href="/contact">
@@ -50,21 +74,19 @@ export default function Hero() {
               TRUSTED PARTNERS:
             </h2>
             <div className="flex space-x-5 font-extralight mb-10">
-              <p>NWCW,</p>
-              <p>Afri,</p>
-              <p>The Pharma Company</p>
+              <p>NWCW</p>
             </div>
           </div>
         </div>
 
-        <div className="bg-hero-second bg-center bg-cover rounded-2xl">
+        <div className="bg-hero-second bg-center bg-cover rounded-2xl hidden md:block">
           <div className="hero-overlay bg-opacity-50  rounded-2xl">
             <h2 className="text-slate-100 text-center font-extralight text-xl pt-3 tracking-widest">
               Start navigating your finance
             </h2>
           </div>
         </div>
-        <div className="bg-hero-third bg-center bg-cover rounded-2xl">
+        <div className="bg-hero-third bg-center bg-cover rounded-2xl hidden md:block">
           <div className="hero-overlay bg-opacity-20  rounded-2xl">
             <h2 className="text-slate-100 text-center font-extralight text-xl pt-3 tracking-widest">
               Securing Futures
